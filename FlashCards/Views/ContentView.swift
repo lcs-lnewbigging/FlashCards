3a775239475110c2f5e81bf89ed632fdeddb0501
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-10 11:41:19 -0500

    Got a static interface started. Showing the question and a button to check the answer

--- FlashCards/Views/ContentView.swift
+++ FlashCards/Views/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            //Show the question
+            Text("Wht's the \"powerhouse\" of a cell?")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            //Check answer
+            Button(action: <#T##() -> Void#>, label: <#T##() -> _#>)
+                .buttonStyle(.bordered)
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.landscapeRight)
     }
 }
